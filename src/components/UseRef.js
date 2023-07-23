78cb0049c7353a97be7ffe5835b39c90fcab63b3
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-07-22 17:28:49 -0700

    useRef to store previous value

--- src/components/UseRef.js
+++ src/components/UseRef.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import { useState, useRef } from 'react'
+import React from "react";
+import { useState, useRef, useEffect } from "react";
 const UseRef = () => {
-    const [name, setName] = useState("")
-    const inputEl = useRef()
-
-    const focus = () => {
-        inputEl.current.focus()
-    }
+  const [name, setName] = useState("");
+  const inputEl = useRef();
+  const prevName = useRef("");
+  const focus = () => {
+    inputEl.current.focus();
+  };
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
   return (
     <div>
-        <div className="name">
-            <input ref={inputEl} value={name} onChange={(e) => setName(e.target.value) }></input>
-            <div>my name is {name}</div>
-            <button style={{marginTop: '10px'}} onClick={focus}>Focus</button>
+      <div className="name">
+        {/* <input ref={inputEl} value={name} onChange={(e) => setName(e.target.value) }></input> */}
+        <input value={name} onChange={(e) => setName(e.target.value)}></input>
+        <div>my name is {name}</div>
+        <button style={{ marginTop: "10px" }} onClick={focus}>
+          Focus
+        </button>
+        <div>
+          My name is {name} and it used to be {prevName.current}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UseRef
+export default UseRef;
