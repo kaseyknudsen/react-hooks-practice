e644686dc0e08db8affdde9ec1da937f02884d7e
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-07-23 19:25:06 -0700

    added random number counter and useRef to hold previous number

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UseRef from './components/UseRef';
 import UseRef2 from './components/UseRef2';
+import UseRef3 from './components/UseRef3';
 function App() {
   return (
     <div className="App">
       <UseRef />
       <UseRef2 />
+      <UseRef3 />
     </div>
   );
 }
---
+++ src/components/UseRef3.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+const UseRef3 = () => {
+  const [name, setName] = useState("");
+  const [counter, setCounter] = useState(0);
+  const inputRef = useRef();
+  const prevNum = useRef("");
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+  const clearNameInput = () => {
+    setName("");
+    focus();
+  };
+  const randomNum = () => {
+    return Math.floor(Math.random() * 101);
+  };
+
+  useEffect(() => {
+    prevNum.current = counter;
+  }, [counter]);
+
+  return (
+    <div style={{ marginTop: "50px" }}>
+      <input
+        type="text"
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <button onClick={clearNameInput}>Reset</button>
+      <div style={{ marginTop: "20px" }}>{counter}</div>
+      <button onClick={() => setCounter(randomNum)} style={{ marginTop: "20px" }}>
+        Click to update counter
+      </button>
+      <div style={{ marginTop: "20px" }}>Previous Number: {prevNum.current}</div>
+    </div>
+  );
+};
+
+export default UseRef3;
