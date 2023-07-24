532fdba682dd3f67caaa86319d68afb2adf99066
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-07-23 20:49:00 -0700

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import UseRef from './components/UseRef';
-import UseRef2 from './components/UseRef2';
-import UseRef3 from './components/UseRef3';
+import "./App.css";
+import UseRef from "./components/UseRef";
+import UseRef2 from "./components/UseRef2";
+import UseRef3 from "./components/UseRef3";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <UseRef />
-      <UseRef2 />
-      <UseRef3 />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UseRef />} />
+          <Route path="useRef2" element={<UseRef2 />} />
+          <Route path="useRef3" element={<UseRef3 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
