b48e4b97d3e415b52c47792c94c3a48cd66b806b
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-07-29 10:04:01 -0700

    json placeholder example of fetching data and displaying it with useEffect

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,21 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const UseEffect = () => {
-    const [resourceType, setResourceType] = useState('posts')
-    useEffect(() => {
-        console.log('render')
-    })
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+  console.log("I will run every time");
+  useEffect(() => {
+    console.log("resource type changed");
+  }, [resourceType]);
+  useEffect(() => {
+    console.log("on mount");
+  }, []);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      //   .then((json) => console.log(json));
+      .then((json) => setItems(json));
+  }, [resourceType]);
   return (
-    
     <>
-    <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
-    </div>
-    <h1>{resourceType}</h1>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map((item) => {
+        return (
+            <pre>{JSON.stringify(item)}</pre>
+        )
+      })}
     </>
-  )
-}
+  );
+};
 
-export default UseEffect
+export default UseEffect;
